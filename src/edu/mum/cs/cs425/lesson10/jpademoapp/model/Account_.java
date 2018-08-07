27127c0f203da0456d952b958197889269e0e4c4
Juan Francisco Mosquera <maxiplux@gmail.com>
2018-08-07 14:03:12 -0500

    Initial commit

---
+++ src/edu/mum/cs/cs425/lesson10/jpademoapp/model/Account_.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.lesson10.jpademoapp.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2018-06-06T17:00:39.572-0500")
+@StaticMetamodel(Account.class)
+public class Account_ {
+	public static volatile SingularAttribute<Account, Integer> accountid;
+	public static volatile SingularAttribute<Account, String> accountname;
+	public static volatile SingularAttribute<Account, String> accountno;
+}
